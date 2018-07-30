cde79bb94f8bee88d9bd77df1327ef90844e7639
Abhishek Bhave <abhishekbhave26@gmail.com>
2018-07-30 21:14:51 +0530

    4th commit

--- Java/Reg_Form.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Reg_Form {
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
-}
