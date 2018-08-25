10c5c893113de09cd9c8dd394e36f50ad95d5366
Abhishek Bhave <abhishekbhave26@gmail.com>
2018-08-25 13:49:08 -0400

    Comments update

---
+++ Python/lambda.py
@@ -0,0 +1,28 @@
+# to demonstrate lambda expression
+
+# check if number is even 
+
+def even(num):
+	if(num%2==0):
+		print('Even')
+	else:
+		print('odd')
+
+
+
+check_even=lambda number:number%2==0
+
+print(check_even(4))
+
+
+#grabs first character of string
+
+check=lambda s:s[0]
+
+print(check('new'))
+
+#reverses string
+
+check_rev=lambda s:s[::-1]
+
+print(check_rev('new'))
