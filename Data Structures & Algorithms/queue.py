37c3afe2c47997096c116a48298871b1e7fd15cd
Abhishek Bhave <abhishekbhave26@gmail.com>
2018-09-25 23:49:07 -0400

    Update

---
+++ Data Structures & Algorithms/queue.py
@@ -0,0 +1,28 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+
+    def size(self):
+        return len(self.items)
+
+    def display(self):
+        return self.items
+
+q=Queue()
+
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.dequeue()
+print(q.display())
+
