b303d7e5d7d6cc321d2d962d8335fae62dbe532a
abhishekbhave26 <abhishekbhave26@gmail.com>
2020-05-22 19:32:21 -0700

    Added frontend to MERN exercise tracker app. Also added contact query form

--- Node.js/Exercise Tracker App/exercise-tracker/README.md
+++ Node.js/Exercise Tracker App/exercise-tracker/README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+Code Credits : https://github.com/beaucarnes/mern-exercise-tracker-mongodb
+
 ## Available Scripts
 
 In the project directory, you can run:
--- Node.js/Exercise Tracker App/exercise-tracker/backend/server.js
+++ Node.js/Exercise Tracker App/exercise-tracker/backend/server.js
@@ -23,9 +23,11 @@ connection.once('open', () => {
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
+const contactRouter = require('./routes/contact');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+app.use('/contact', contactRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
