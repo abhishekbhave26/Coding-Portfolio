36f0336419ce6ff09798036a80fecd2d5337bd2a
abhishekbhave26 <abhishekbhave26@gmail.com>
2020-09-20 17:50:31 -0700

    Coding portfolio update

--- Python/snowflake.py
+++ Python/snowflake.py
@@ -12,3 +12,22 @@ def push(value,result=[]):
 first=push(0)
 push(1,first)
 print(push(2))
+
+
+import math
+
+millnames = ['',' Thousand',' Million',' Billion',' Trillion']
+
+def millify(n):
+    n = float(n)
+    millidx = max(0,min(len(millnames)-1,
+                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+
+    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
+
+x=24
+i=1626
+while i<=2012:
+    x*=2
+    i+=8
+print(millify(x))
